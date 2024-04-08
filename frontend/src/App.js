f6291dd7fa49352f04a99cdd26ab59367dd474b4
Shivansh Chhabra <20603692+shan18u@users.noreply.github.com>
2024-04-07 21:56:04 -0700

    add generate form to conect with flask, need more testing with redis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,17 +9,34 @@ import './App.css';
 
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<TitlePage />} />
-        <Route path='login' element={<LoginPage/>} />
-        <Route path='signup' element={<SignUpPage/>} />
-        <Route path='home' element={<HomePage/>} />
-        <Route path='generate' element={<Generate/>} />
-      </Routes>
-    </Router>
-  );
+  useEffect(() => {
+    fetch('/api') // proxy to our flask backend
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+    // maybe need tp import some data from flask to react to test Div out.
+
+    return (
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<TitlePage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/signup' element={<SignUpPage />} />
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/generate' element={<Generate />} />
+          </Routes>
+        </Router>
+        <div>
+          <h1>Hello, React!</h1>
+          <p>Data from Flask: {JSON.stringify(data)}</p>
+          <div className="App">
+            <RecipeForm />
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default App;
