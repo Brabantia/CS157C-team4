d6b920b19594c89127d224ef4eaa30cb8b9523c8
Shivansh Chhabra <20603692+shan18u@users.noreply.github.com>
2024-04-21 13:11:38 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,29 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import Generate from './pages/Generate';
-
-import './App.css';
+import Recipes from './pages/Recipes'
+import Instructions from './pages/Instructions'
+import Unedible from './pages/unedible';
+import Results from './pages/Results'
+import './App.css'; //not needed
 
 function App() {
-    return (
-        <Router>
-          <Routes>
-            <Route path="/" element={<TitlePage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/signup' element={<SignUpPage />} />
-            <Route path='/home' element={<HomePage />} />
-            <Route path='/generate' element={<Generate />} />
-          </Routes>
-        </Router>
-    );
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<TitlePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/generate' element={<Generate />} />
+          <Route path='/instructions' element={<Instructions />} />
+          <Route path='/recipes' element={<Recipes />} />
+          <Route path='/unedible' element={<Unedible />} />
+          <Route path='/results' element={<Results />} />
+
+        </Routes>
+      </Router>
+  );
 }
 
 export default App;
